a3e5998e96cf3cab788a9b620459a0ca31c2d060
wrenr <wreynolds@oit.ucla.edu>
2014-12-16 22:52:52 -0800

    move SASS config to root and update paths

--- config.rb
+++ config.rb
@@ -2,12 +2,12 @@
 require 'sass-css-importer'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "css/fonts"
+http_path = "www/"
+css_dir = http_path+"css"
+sass_dir = http_path+"sass"
+images_dir = http_path+"img"
+javascripts_dir = http_path+"js"
+fonts_dir = http_path+"css/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -24,7 +24,7 @@ relative_assets = true
 # Example: @import "CSS:library/some_css_file"
 require 'sass-css-importer'
 # specify the absolute path to the css folder for CssImporter.
-add_import_path Sass::CssImporter::Importer.new("css")
+add_import_path Sass::CssImporter::Importer.new(http_path+"css")
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
