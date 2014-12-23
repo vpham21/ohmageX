918af3b3ac9296371c3c43eeaf272aa9e80d9f97
wrenr <wreynolds@oit.ucla.edu>
2014-12-22 16:53:04 -0800

    Use body `touchmove` event to disable and re-enable scrolling in mobile views

--- www/blocks/component/_slide-out.js
+++ www/blocks/component/_slide-out.js
@@ -19,16 +19,31 @@ var SlideOutComponent = function(ele, context, activationEvent){
         $ele.attr('data-state', active);
         // add body 'slideout-state' so body can be modified when slideout is active.
         $('body').attr('slideout-state', active);
+
+        if (active === false) {
+            // Re-enable scrolling in mobile views.
+            $('body').off('touchmove.slideout');
+        } else {
+            // Disable scrolling in mobile views.
+            $('body').on('touchmove.slideout', function(e) {
+                e.preventDefault();
+            });
+        }
     };
 
     __self__.open = function(){
         $ele.attr('data-state', 'active');
         $('body').attr('slideout-state', 'active');
+
+        $('body').on('touchmove.slideout', function(e) {
+            e.preventDefault();
+        });
     };
 
     __self__.close = function(){
         $ele.attr('data-state', false);
         $('body').attr('slideout-state', false);
+        $('body').off('touchmove.slideout');
     };
 
     __self__.toggleOn = function(ele, context){
