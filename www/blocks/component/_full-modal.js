963e79d9d9176563e8844ebd2af5335cbbb3d8c3
wrenr <wreynolds@oit.ucla.edu>
2015-08-27 00:36:52 -0700

    add basic full modal component - just includes show and hide methods

---
+++ www/blocks/component/_full-modal.js
@@ -0,0 +1,25 @@
+var FullModalComponent = function(ele, context){
+
+    var $ele = $(context ? context : 'body').find(ele),
+        __self__ = this;
+
+    __self__.toggle = function(){
+        __self__[$ele.attr('data-visible') == 'false' ? 'show' : 'hide']();
+    };
+
+    __self__.hide = function(){
+        $ele.attr('data-visible', 'false');
+    };
+
+    __self__.show = function(){
+        $ele.attr('data-visible', 'true');
+        $ele.focus();
+    };
+
+    __self__.toggleOn = function(event, ele, context){
+        $(context ? context : 'body').find(ele).on(event, function(){
+            __self__.toggle();
+        });
+    };
+
+};
